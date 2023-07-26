971ae0394d85328ee5002538c9828a43e82c057f
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2023-07-26 22:47:41 +0100

    Basic cache

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+""" A basic dictionary using a class """
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ A basic cache class. """
+    def __init__(self):
+        """ Initializes the class based on the super class. """
+        super().__init__()
+
+    def put(self, key, item):
+        """ Adds an item to the dictionary """
+        if not key or not item:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ Gets value stored at a particular key. """
+        if not key or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
