7c3e1d261614eb8468483a957d2d775462163e72
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2023-08-01 12:49:48 +0100

    Find best language match

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -19,7 +19,7 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
-@app.localeselector
+@babel.localeselector
 def get_locale():
     """ Configures language translation to get best match  """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
