ff212fdd6f0ef18ac8f8cb94abbf32f2546c820f
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2023-08-01 17:30:01 +0100

    Forcing locale

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+""" Internalization """
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """Configures the babel extension"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """ Configures language translation to get best match  """
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
+    else:
+        return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """Landing page"""
+    return render_template('4-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
